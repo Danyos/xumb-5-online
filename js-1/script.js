dd25bb37769e25a99fe746c936eb2609eb37f22d
Danyos <daniel.hambardzumyan@mail.ru>
2023-03-10 21:59:05 +0400

    number and false

---
+++ js-1/script.js
@@ -0,0 +1,141 @@
+// let x=7
+// number
+// 1 2 3 4 5 6 7 8 9
+// +,-,*,/,
+// **
+// console.log(5**3)//5*5*5
+
+// %
+// 10%4 // 2
+// 10%6 // 4
+// 10%2 // 0
+// 10%6 // 4
+// 10%4 // 2
+
+// >
+
+// >=
+// <=
+// <=
+
+// ==
+// ===
+
+// console.log(50)
+// console.log('100')
+
+
+// ___________orinak________________
+// x=x**2
+// vs
+// x**=2
+// x=x+5
+// x*=5
+// let x1=7//7
+// let x2=5//5
+// let x3=0
+//
+// x3=x1//7
+// x1=x2//5
+// x2=x3
+//
+//
+// console.log(x1)//7
+// console.log(x2)//5
+
+
+
+// let t=8
+// + nshany tarber deve e iren pahum
+//1. + nshany gumarum e
+//2. artahaytutyunic araj drvac + nshany sarqum e number +"7" //7
+// 3. ++ vory nshanakum e artahytutyun t+=1
+// 4. + nshany stringi depqum pahum e iren vorpes concat
+// number+string=string
+
+// console.log(""+(7+5))
+
+// "barev "+7
+// 'barev 7'
+// "10"+"50" // '1050' //string
+// +"10" + +"50"
+// 60 //number
+//
+// t++//t+=1
+// ++t //t+=1
+// console.log(t)
+// console.log(t++)
+// console.log(t)
+// console.log(++t)
+// console.log(t)
+
+
+// let price='100$'
+
+// let y
+// console.log(y)//undefined
+// !true
+// false
+// !1
+// false
+// !false
+// true
+// 5===5
+// true
+// 5!=5
+// false
+// 5!=6
+// true
+// ___________false__________Arjeqner___________
+// Boolean(false) / false
+// Boolean(1) // true
+// Boolean(0) // false
+// Boolean(0) // false
+// Boolean(null) // false
+// Boolean(undefined) //false
+// Boolean("") false
+// greladev
+// !!""
+// false
+// !!null
+// false
+
+// ________________________________
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// tnayin
+
+let a=5
+let b=7
+
+
+
+
+// console.log(a)//
+// console.log(b)//
+
+
+// x1-=x2//2
+// x1=x2-x1//-2
+
+// console.log(x)
+
+
+// __________________________
+
+// https://learn.javascript.ru/comparison
+// https://learn.javascript.ru/logical-operators
+// https://learn.javascript.ru/nullish-coalescing-operator
