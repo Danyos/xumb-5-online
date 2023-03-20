f335072e09c0b589a29686d37eddb09c9b39d1f7
Danyos <daniel.hambardzumyan@mail.ru>
2023-03-20 22:33:23 +0400

    if

---
+++ js-5/script.js
@@ -0,0 +1,132 @@
+// let age=prompt('Քո տարիքը?');
+//
+//
+// if(age<18){
+//
+//     alert('Դուք չեք կարող մուտք գործել,քանի որ անչափահաս եք');
+// }else if(age>=18 && age<=63){
+//     alert('Շնորհավորում ենք, դուք բարեկաջող գրանցվել եք');
+// }else if(age>63){
+//     alert('դուք տարիքով մեծ եք');
+// }
+
+// let a = 7;
+// let b = 21;
+// let c = 14;
+
+// let idram="50000"
+// let visa="17250"
+//
+// let product="8500 dram"
+// let info=''
+// if(confirm("du cankanum eq idramov  vcharel") && idram>=parseFloat(product)){
+//
+//
+//     idram-=parseFloat(product)
+//     info='idram'
+//
+// }else if(confirm("du cankanum eq visa cartov vcharel") && visa>=parseFloat(product)){
+//
+//     visa-=parseFloat(product)
+//     info='visa'
+// }
+
+
+// if(info==='idram'){
+//     alert("gandzumy katarvel e idram havelvacic")
+// }else if(info==='visa'){
+//     alert("gandzumy katarvel e cartic havelvacic")
+// }else{
+//     alert("Anbavarar mijocner "+" dram:"+ idram+ " visa:"+visa)
+// }
+
+
+
+// 3 biz
+// 5 fiz
+// 3 ham 5 fizbiz
+let num=9
+// let res=null
+// if(num%3===0 && num%5===0){
+//     res="bizfiz"
+// }else if(num%3===0){
+//     res="biz"
+// }else if(num%5===0){
+//     res="fiz"
+// }
+// console.log(res)
+
+// v2
+
+// let res=null
+// if(num%3===0 && num%5===0){
+//     res="bizfiz"
+// }else{
+// res="sxal"
+// }
+// let res=(num%3===0 && num%5===0)?"bizfiz"
+//     :(num%3===0)?"biz"
+//         :(num%5===0)?"fiz"
+//             :"sxal"
+// console.log(res)
+// let how=confirm("Duq  cankanum eq karmir tpi")
+
+// document.body.style.background=how?"red":"black"
+// if(how){
+//     document.body.style.background="red"
+// }else{
+//     document.body.style.background="black"
+// }
+let num2="75"
+// if(parseFloat(num2) === 74){
+//     alert('74')
+// }else if(parseFloat(num2) === 75){
+//     alert('75')
+// }else if(parseFloat(num2) === 76){
+//     alert('76')
+// }else if(parseFloat(num2) === 77){
+//     alert('77')
+// }else{
+//     alert(1)
+// }
+switch (num2) {
+    case "74":
+        alert('74')
+        break
+    case "75":
+        alert('75')
+        break
+    case "76":
+        alert('76')
+        break
+    case "77":
+        alert('77')
+        break
+    default:alert('sxal')
+}
+
+
+
+
+
+
+
+
+// 1
+
+// unenal 3 prompt
+// 1 prompum grel tivy  // 10
+// 2 promputm gortcoxutyan nshany  // +
+// 3 rd grel eli tivy  // 20
+// erb greq verjacneq tpi resultaty // 30
+// orink 10 + 20 =30
+
+
+// 2
+// let login="user123"
+// let psw="group123456"
+
+// unenq 2 prompt meky logini hmar myusy passwordi
+// ayd arjenqery hamamemateq login ev psw het ete chisht e asi okey ete voch asi sxal
+// *  kap chuni te oginy metsatar kjam poqratar kgres // USER123 asi maka chishta
+// bayc passwordi depqum vonc grvac e aydpes lini //group123456 
