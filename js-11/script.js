f4d36b2803ceff970b93d75ff42185c225c0da7a
Danyos <daniel.hambardzumyan@mail.ru>
2023-04-03 22:21:01 +0400

    array

---
+++ js-11/script.js
@@ -0,0 +1,57 @@
+// let str = prompt("grel anun")
+// let strDb = false
+// for (let i = 0; i < str.length; i++) {
+//     let code = str.charCodeAt(i);
+//     if ((code >= 65 && code <= 122) || (code >= 48 && code <= 57)) {
+//         strDb = true
+//     } else {
+//         strDb = false
+//         break
+//     }
+// }
+// console.log(strDb)
+
+let a1=[1,undefined,3,false,4,"5","hello",true,6,null,8,"700$"]
+
+// a1[a1.length]=parseFloat(a1[a1.length-1])+100+"$"
+// let A1Index=a1.indexOf(false)
+// a1.splice(A1Index,2,"Deleted")
+// let s=a1.slice(1,4)
+
+// a1.push(77)
+// a1.pop()
+// a1.shift()
+// a1.unshift(789456)
+
+a1.reverse()
+
+
+console.log(a1)
+
+
+
+
+
+
+
+
+
+
+
+// let str="barev"
+// str[0]="B"
+// console.log(str)
+
+
+
+
+// for (let i=0;i<a1.length;i++){
+//     // console.log(a1.at(i))
+//     if((typeof a1[i]==='number' || typeof a1[i]==='string') && !isNaN(parseFloat(a1[i]))){
+//         console.log(a1[i])
+//     }
+//
+// }
+
+
+
