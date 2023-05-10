a43104a8d02331381ddc4ac9bb6a439b738be8de
danyos <daniel.hambardzumyan@mail.ru>
2023-05-10 22:35:58 +0400

    setInterval

--- js-20/script.js
+++ js-20/script.js
@@ -2,38 +2,20 @@ let answer=document.querySelector('#answer')
 let again=document.querySelector('.again')
 let btn=document.querySelector('.btn')
 let text=document.querySelector('.text')
+let seconds=document.querySelector('.seconds')
+let minseconds=16
+let x
 let fruct=["Emma",
-    "Olivia",
-    "Ava",
-    "Isabella",
-    "Sophia",
-    "Charlotte",
-    "Mia",
-    "Amelia",
-    "Harper",
-    "Evelyn",
-    "Abigail",
-    "Emily",
-    "Elizabeth",
-    "Mila",
-    "Ella",
-    "Avery",
-    "Sofia",
-    "Camila",
-    "Aria",
-    "Scarlett",
-    "Victoria",
-    "Madison",
-    "Luna",
-    "Grace",
-    "Chloe",
-    "Penelope",
-    "Layla",
-    "Riley",
-    "Zoey",
-    "Nora"]
+    "asd",
+    "zxc",
+    "qwer",
+
+
+]
 fruct=fruct.sort(()=> 0.4-Math.random())
+
 let f_index=0
+
 console.log(fruct)
 let hashiv={
     succses:0,
@@ -49,17 +31,32 @@ function question(){
             <h6>Worning: ${hashiv.worning} </h6>
  
         `
+        clearInterval(x)
         btn.style.display='none'
         answer.style.display='none'
         again.style.display='block'
         return
     }
+
     let arr=res.split('')
     arr=arr.sort(()=> 0.3-Math.random()).join('')
-
+    minseconds=16
     text.innerHTML=arr
+
 }
 question()
+timer()
+function timer(){
+     x=setInterval(()=>{
+        if(minseconds===0){
+            next()
+            minseconds=16
+        }
+        seconds.innerHTML=minseconds
+        minseconds--
+    },100)
+}
+
 function tryagain(){
     f_index=0
     hashiv.succses=0
@@ -68,6 +65,7 @@ function tryagain(){
     answer.style.display='block'
     again.style.display='none'
     question()
+    timer()
 }
 function next(){
    if(fruct[f_index].toLowerCase()===answer.value.toLowerCase()){
