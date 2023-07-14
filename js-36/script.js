c05f07dfd70951c6ee03f79e5d736b20426d1b33
danyos <daniel.hambardzumyan@mail.ru>
2023-07-14 22:13:58 +0400

    jquery

---
+++ js-36/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    // $('.mytext2').slideUp()
+    // $('.btn').click(function () {
+    //     $('.mytext').slideToggle("slow",function (){
+    //         $('.mytext2').slideToggle()
+    //     })
+$('.atage').click(function (){
+    console.log(this)
+    $(this).hide()
+})
+    // })
+   // $(this).click(function (e){
+   //     e.target.style.display='none'
+   // })
+})
+// $("input").keypress(function(){
+//     console.log(this.value)
+// });
